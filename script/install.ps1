32cf4d9b6cf60b75718ed35fed66e5644eed293a
mafuka <i@mafuka.com>
2023-11-07 20:21:36 +0800

    refactor(script): better Windows installation script

--- script/install.ps1
+++ script/install.ps1
@@ -1,21 +1,78 @@
-$repoURL = "https://ghproxy.com/github.com/mafuka/wylogger"
-$arch = if ([System.Environment]::Is64BitOperatingSystem) { "amd64" } else { "386" }
-$releaseName = "wylogger_windows_" + $arch + ".zip"
-$releaseURL = $repoURL + "/releases/latest/download/" + $releaseName
+$projectName = "wylogger"
 
-$tempPath = [System.IO.Path]::GetTempPath()
-$downloadFile = Join-Path -Path $tempPath -ChildPath "wylogger.zip"
-$userHome = [System.Environment]::GetFolderPath('MyDocuments')
-$xdnlPath = Join-Path -Path $userHome -ChildPath "wylogger"
+# Check if installed.
+try {
+    $commandInfo = Get-Command wylogger -ErrorAction Stop
+    Write-Host "$projectName is already in $($commandInfo.Source), the installation will now terminate, press Enter to exit..."
+    Read-Host
+    exit 0
+} catch {
+    Install-Package
+}
 
-Invoke-WebRequest -Uri $releaseURL -OutFile $downloadFile
+function Install-Package {
+    # Check system architecture.
+    $arch = $env:PROCESSOR_ARCHITECTURE
+    $archWow = $env:PROCESSOR_ARCHITEW6432
 
-Expand-Archive -Path $downloadFile -DestinationPath $xdnlPath -Force
+    if ($arch -eq "AMD64" -or $archWow -eq "AMD64") {
+        $architecture = "amd64"
+    } elseif ($arch -eq "x86" -and -not $archWow) {
+        $architecture = "386"
+    } elseif ($arch -eq "ARM64") {
+        $architecture = "arm64"
+    } else {
+        $architecture = "unknown"
+        Write-Host "The system architecture could not be identified, the installation will now terminate, press Enter to exit..."
+        Read-Host
+        exit 1
+    }
 
-$configExamplePath = Join-Path -Path $xdnlPath -ChildPath "config.example.yml"
-$configPath = Join-Path -Path $xdnlPath -ChildPath "config.yml"
-Copy-Item -Path $configExamplePath -Destination $configPath -Force
 
-Write-Host "Please edit the configuration file at $configPath with your preferred text editor."
+    Write-Host "Architecture: $architecture"
 
-Start-Process "notepad.exe" -ArgumentList $configPath
+    # Get package url.
+    $repoURL = "https://ghproxy.com/github.com/mafuka/wylogger"
+    $os = "windows"
+    $packageName = $projectName + "_" + $os + "_" + $arch + ".zip"
+    $packageURL = $repoURL + "/releases/latest/download/" + $packageName
+
+    Write-Host "Package URL: $packageURL"
+
+    # Get temp dir.
+    $tempDir = [System.IO.Path]::GetTempPath()
+    Write-Host "Temp directory: $tempDir"
+
+    # Get download dir.
+    $downloadPath = Join-Path -Path $tempDir -ChildPath "wylogger.zip"
+    Write-Host "Download package as: $downloadPath"
+
+    # Get install dir.
+    $installDir = Join-Path -Path $home -ChildPath "wylogger"
+    Write-Host "Install package to: $installDir"
+
+    # Download package.
+    Write-Host "Start downloading the package..."
+    Invoke-WebRequest -Uri $packageURL -OutFile $downloadPath
+
+    # Unzip package.
+    Expand-Archive -Path $downloadFile -DestinationPath $installDir -Force
+
+    # Copy profile.
+    $profileExamplePath = Join-Path -Path $installDir -ChildPath "config.example.yml"
+    $profilePath = Join-Path -Path $installDir -ChildPath "config.yml"
+    Copy-Item -Path $profileExamplePath -Destination $profilePath -Force
+
+    # Set user env PATH.
+    $currentPATH = [Environment]::GetEnvironmentVariable("PATH", [EnvironmentVariableTarget]::User)
+    $newPATH = currentPATH + ";" + $installDir
+    [Environment]::SetEnvironmentVariable("PATH", $newPATH, [EnvironmentVariableTarget]::User)
+
+    # Open profile.
+    Start-Process "notepad.exe" -ArgumentList $configPath
+
+    # End.
+    Write-Output "$projectName has been installed to $installDir, the profile is open for modification, the installer will now conclude, press Enter to exit..."
+    Read-Host
+    exit 0
+}
